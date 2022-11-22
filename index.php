f61f8839c67058d6789c3dab9951ef157798d93e
Oyinkansola Shoroye <oyinkansolashoroye@gmail.com>
2022-11-22 17:58:41 +0100

    I added some texts

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Hello Teqbaddie";
+$text = "Hello World";
+$name = "I am Oyinkansola Shoroye - Teqbaddie";
+$role = "I am a Software Developer, currently learning PHP";
+
+
+echo $text . "<br>";
+echo $name . "<br>";
+echo $role . "<br>";
 
 ?>
